84923cba8295fb3d9987227003272741458737f4
Windeko <tylerjameswoody@gmail.com>
2024-11-19 11:40:12 -0800

    Add project files.

---
+++ HabitTracker.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace HabitTracker.Core;
+
+public class Class1
+{
+}
---
+++ HabitTracker.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace HabitTracker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
